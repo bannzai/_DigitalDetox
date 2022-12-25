5ec9d2c567a7f2b49aaf98c3abe03b091a78f9d0
bannzai <kingkong999yhirose@gmail.com>
2022-12-25 21:30:23 +0900

    Initial Commit

---
+++ SocialDetox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SocialDetox
+//
+//  Created by bannzai on 2022/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialDetox/SocialDetoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialDetoxApp.swift
+//  SocialDetox
+//
+//  Created by bannzai on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialDetoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
