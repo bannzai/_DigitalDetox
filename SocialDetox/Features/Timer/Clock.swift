c51f5ac2d6bdbe1122010caaef287c50a18c808e
bannzai <kingkong999yhirose@gmail.com>
2022-12-28 21:57:47 +0900

    Fix countdown

---
+++ SocialDetox/Features/Timer/Clock.swift
@@ -0,0 +1,26 @@
+import Combine
+import SwiftUI
+
+class _Clock: ObservableObject {
+    @Published var now: Date = .now
+
+    let timer = Timer.publish(every: 1, on: .main, in: .default)
+        .autoconnect()
+
+    init() {
+        timer.assign(to: &$now)
+    }
+
+    deinit {
+        timer.upstream.connect().cancel()
+    }
+}
+
+@propertyWrapper
+struct Clock: DynamicProperty {
+    @StateObject private var clock = _Clock()
+
+    var wrappedValue: _Clock {
+        clock
+    }
+}
---
+++ SocialDetox/Features/Timer/Countdown.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct Countdown: View {
+    @Clock var clock
+    let goal: Date
+
+    var body: some View {
+        let components = dateComponents()
+
+        HStack(spacing: 10) {
+            if let hour = components?.hour {
+                Number(value: hour)
+                Colon()
+            }
+            if let minute = components?.minute {
+                Number(value: minute)
+                Colon()
+            }
+            if let second = components?.second {
+                Number(value: second)
+            }
+        }
+    }
+
+    func dateComponents() -> DateComponents? {
+        Calendar.autoupdatingCurrent.dateComponents([.hour, .minute, .second], from: clock.now, to: goal)
+    }
+
+    struct Number: View {
+        let value: Int
+
+        var body: some View {
+            Text(String(format: "%02d", value))
+                .font(.system(size: 28, weight: .semibold))
+        }
+    }
+
+    struct Colon: View {
+        var body: some View {
+            Text(":")
+                .font(.system(size: 28, weight: .semibold))
+        }
+    }
+}
+
+struct Countdown_Previews: PreviewProvider {
+    static var previews: some View {
+        Countdown(goal: .now.addingTimeInterval(60 * 30))
+    }
+}
