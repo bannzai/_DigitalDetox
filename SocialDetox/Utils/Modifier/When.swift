81fe55f1c2247a1ff4cc93a09b6cae73460fa834
bannzai <kingkong999yhirose@gmail.com>
2022-12-29 00:35:17 +0900

    Rename to when

--- SocialDetox/Utils/Modifier/When.swift
+++ SocialDetox/Utils/Modifier/When.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension View {
-  @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+  @ViewBuilder func when<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
     if condition {
       transform(self)
     } else {
