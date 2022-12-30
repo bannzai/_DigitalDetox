360153d3a6e8130df4dd548c5430a65e2056e188
bannzai <kingkong999yhirose@gmail.com>
2022-12-30 21:38:12 +0900

    Add localization

--- SocialDetox/ContentView.swift
+++ SocialDetox/ContentView.swift
@@ -1,61 +1,20 @@
-//
-//  ContentView.swift
-//  SocialDetox
-//
-//  Created by bannzai on 2022/12/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-  @Environment(\.displayScale) var displayScale
-  @EnvironmentObject var pip: PiP
-  @Clock var clock
-  @State var remainingTime: TimeInterval? = 60 * 30
-
   var body: some View {
-    let countdown = Countdown(remainingTime: remainingTime)
-    VStack {
-      if pip.canStart {
-        PiPContainer()
-          .onChange(of: clock.now) { _ in
-            switch pip.progress {
-            case .willStart, .didStart, .willStop:
-              if let remainingTime {
-                self.remainingTime = remainingTime - 1
-              }
-            case nil, .didStop:
-              break
-            }
-
-            pip.enqueue(content: countdown, displayScale: displayScale)
-          }
-          .frame(width: pip.size.width, height: pip.size.height)
-          .border(Color.yellow)
-
-
-        if pip.isActivated {
-          Button {
-            pip.stop()
-          } label: {
-            Image(systemName: "stop.fill")
-              .imageScale(.large)
-              .foregroundColor(.accentColor)
-          }
-        } else {
-          Button {
-            pip.start()
-          } label: {
-            Image(systemName: "play.fill")
-              .imageScale(.large)
-              .foregroundColor(.accentColor)
-          }
-        }
-      } else {
-        ProgressView()
+    List {
+      Section("Header 1") {
+        Text("1")
+        Text("2")
+        Text("3")
+      }
+      Section("Header 2") {
+        Text("1")
+        Text("2")
+        Text("3")
       }
     }
-    .background(Color.red)
+    .listStyle(.insetGrouped)
   }
 }
 
