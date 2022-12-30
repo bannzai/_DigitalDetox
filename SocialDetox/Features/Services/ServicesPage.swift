895a415ba3d08a34265b6f31d121259837ffb694
bannzai <kingkong999yhirose@gmail.com>
2022-12-30 22:11:09 +0900

    WIP

--- SocialDetox/Features/Services/ServicesPage.swift
+++ SocialDetox/Features/Services/ServicesPage.swift
@@ -31,7 +31,7 @@ struct ServicesView: View {
   }
 }
 
-enum Service: Int, Identifiable {
+enum Service: Int, Identifiable, CaseIterable {
   // SNS
   case twitter, facebook, instagram, snapchat
   // Movie
@@ -40,8 +40,52 @@ enum Service: Int, Identifiable {
   case slack, discord
 
   var id: RawValue { rawValue }
+
+  var name: String {
+    "\(self)".uppercased()
+  }
+
+  var iconName: String {
+    "\(self)"
+  }
+
+  enum Category: Int, Identifiable, CaseIterable {
+    case sns
+    case video
+    case message
+
+    var id: RawValue { rawValue }
+
+    var titleKey: LocalizedStringKey {
+      switch self {
+      case .sns:
+        return "SNS"
+      case .video:
+        return "Video"
+      case .message:
+        return "Message"
+      }
+    }
+
+    var services: [Service] {
+      Service.allCases.filter { $0.category == self }
+    }
+  }
+
+  var category: Category {
+    switch self {
+    case .twitter, .facebook, .instagram, .snapchat:
+      return .sns
+    case .youtube, .netflix:
+      return .video
+    case .slack, .discord:
+      return .message
+    }
+  }
 }
 struct ServiceView: View {
+  let service: Service
+
   var body: some View {
     HStack {
 
