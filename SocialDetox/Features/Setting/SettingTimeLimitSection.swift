ee3cb5e0af4dbd8ad305a29be171c8d74af158b4
bannzai <kingkong999yhirose@gmail.com>
2022-12-31 13:43:19 +0900

    Fix

--- SocialDetox/Features/Setting/SettingTimeLimitSection.swift
+++ SocialDetox/Features/Setting/SettingTimeLimitSection.swift
@@ -1,33 +1,114 @@
 import SwiftUI
 
 struct SettingTimeLimitSection: View {
-  @AppStorage(.sns) private var _sns: TimeInterval = .defaultTimeLimit
-  @AppStorage(.video) private var _video: TimeInterval = .defaultTimeLimit
-  @AppStorage(.message) private var _message: TimeInterval = .defaultTimeLimit
-
-  var sns: Binding<Date> {
-    .init(get: { Date(timeIntervalSince1970: _sns) }, set: { _sns = $0.timeIntervalSince1970 })
-  }
-  var video: Binding<Date> {
-    .init(get: { Date(timeIntervalSince1970: _video) }, set: { _video = $0.timeIntervalSince1970 })
-  }
-  var message: Binding<Date> {
-    .init(get: { Date(timeIntervalSince1970: _message) }, set: { _message = $0.timeIntervalSince1970 })
+  struct Time: Hashable {
+    let hour: Int
+    let minute: Int
   }
 
+  @AppStorage(.snsHour) private var snsHour: Int = .defaultHour
+  @AppStorage(.snsMinute) private var snsMinute: Int = .defaultMinute
+  @AppStorage(.videoHour) private var videoHour: Int = .defaultHour
+  @AppStorage(.videoMinute) private var videoMinute: Int = .defaultMinute
+  @AppStorage(.messageHour) private var messageHour: Int = .defaultHour
+  @AppStorage(.messageMinute) private var messageMinute: Int = .defaultMinute
+
+  @State var snsHourPickerIsPresented = false
+  @State var snsMinutePickerIsPresented = false
   var body: some View {
     Section {
-      DatePicker("SNS", selection: sns, displayedComponents: .hourAndMinute)
-        .datePickerStyle(.wheel)
-
-      DatePicker("Video", selection: video, displayedComponents: .hourAndMinute)
-        .datePickerStyle(.wheel)
-
-      DatePicker("Message", selection: message, displayedComponents: .hourAndMinute)
-        .datePickerStyle(.wheel)
+      Component(
+        titleKey: "SNS",
+        hour: $snsHour,
+        minute: $snsMinute
+      )
+      Component(
+        titleKey: "Video",
+        hour: $videoHour,
+        minute: $videoMinute
+      )
+      Component(
+        titleKey: "Message",
+        hour: $messageHour,
+        minute: $messageMinute
+      )
     } header: {
       Text("Timer")
         .textCase(nil)
+    }
+  }
+
+  struct Component: View {
+    let titleKey: LocalizedStringKey
+    let hour: Binding<Int>
+    let minute: Binding<Int>
+
+    @State var hourPickerIsPresented = false
+    @State var minutePickerIsPresented = false
+
+    var body: some View {
+      VStack(spacing: 8) {
+        HStack {
+          Text(titleKey)
+          Spacer()
+          HStack(spacing: 10) {
+            Button {
+              hourPickerIsPresented.toggle()
+            } label: {
+              Text("\(hour.wrappedValue)")
+            }
+            .buttonStyle(.bordered)
+
+            Button {
+              minutePickerIsPresented.toggle()
+            } label: {
+              Text(String(format: "%02d", minute.wrappedValue))
+            }
+            .buttonStyle(.bordered)
+          }
+        }
+
+        if hourPickerIsPresented {
+          Divider()
+          VStack(alignment: .leading, spacing: 4) {
+            HStack {
+              Text("Hour")
+              Spacer()
+              Button {
+                hourPickerIsPresented = false
+              } label: {
+                Text("Close")
+              }
+            }
+            Picker("", selection: hour) {
+              ForEach(0..<10) { hour in
+                Text("\(hour)")
+              }
+            }
+            .pickerStyle(.wheel)
+          }
+        }
+        if minutePickerIsPresented {
+          Divider()
+          VStack(alignment: .leading, spacing: 4) {
+            HStack {
+              Text("Minute")
+              Spacer()
+              Button {
+                minutePickerIsPresented = false
+              } label: {
+                Text("Close")
+              }
+            }
+            Picker("", selection: minute) {
+              ForEach(0..<60) { minute in
+                Text("\(minute)")
+              }
+            }
+            .pickerStyle(.wheel)
+          }
+        }
+      }
     }
   }
 }
@@ -46,12 +127,15 @@ struct SettingTimeLimitSection_Previews: PreviewProvider {
 // MARK: - Private
 fileprivate extension String {
   static let prefix = "Setting.LimitTime"
-  static let sns = "\(prefix).sns"
-  static let video = "\(prefix).video"
-  static let message = "\(prefix).message"
+  static let snsHour = "\(prefix).snsHour"
+  static let snsMinute = "\(prefix).snsMinute"
+  static let videoHour = "\(prefix).videoHour"
+  static let videoMinute = "\(prefix).videoMinute"
+  static let messageHour = "\(prefix).messageHour"
+  static let messageMinute = "\(prefix).messageMinute"
 }
 
-fileprivate extension TimeInterval {
-  // Default time is 30 min
-  static let defaultTimeLimit = Date(timeIntervalSince1970: 0).addingTimeInterval(30 * 60).timeIntervalSince1970
+fileprivate extension Int {
+  static let defaultHour = 0
+  static let defaultMinute = 30
 }
